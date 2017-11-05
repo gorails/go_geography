7df852a3e0c361c1d2e3515f5f3ab22f71135721
João Carlos Ottobboni <jcottobboni@gmail.com>
2017-11-05 19:10:29 -0200

    Initial commit

---
+++ lib/go_geography/version.rb
@@ -0,0 +1,3 @@
+module GoGeography
+  VERSION = '0.1.0'
+end
---
+++ test/go_geography_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GoGeography::Test < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, GoGeography
+  end
+end
